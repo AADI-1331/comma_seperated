2a603efd7d410e257fc76c2cb80fdb5bbeaca114
Yash Bobde <ymbobde99@gmail.com>
2021-09-30 23:36:41 +0530

    Update comma_separated.py
    review

--- comma_seperated.py
+++ comma_seperated.py
@@ -1,11 +1,14 @@
 #Hints:
 #In case of input data being supplied to the question, it should be assumed to be a console input.
 #tuple() method can convert list to tuple
-#
-
-Solution:
+# Solution goes like this: 
+ 
 values=raw_input()
+
 comma_form=values.split(",")
+
 tuple_form=tuple(comma_form)
+
 print comma_form
+
 print tuple_form
